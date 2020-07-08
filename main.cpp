80ef93bde74541cae173892313a1ce5b4a07f85c
shantanu-create <snehal.khandve07@gmail.com>
2020-07-08 14:03:04 +0530

    cut and copy done

--- main.cpp
+++ main.cpp
@@ -32,9 +32,115 @@ int main(int argc , char* argv[]) {
         cout << "File added successfully!!!\n";
     }
     else if((operation.compare("copy")) == 0) {
+        string old_filename = argv[2];
+        string new_filename = "copy_" + old_filename;
+        
+        fout.open(new_filename);
+        if(!fin) {
+            cout << "\n Invalid File Name. \n There is no such File or Directory ...";
+            exit(EXIT_FAILURE);
+        }
+        fin.open("aggregator.txt" , ios::app);
+        if(!fout) {
+            cout <<"\n Invalid File Name. \n There is no such File or Directory ...";
+            exit(EXIT_FAILURE);
+        }
+
+        string text;
+        bool file_found = false;
+        while(getline(fin, text)) {
+             if((text.compare(argv[2])==0) && !file_found)
+             {
+                 file_found = true;
+                 while (getline(fin,text))
+                 {
+                    if(text.compare("//")==0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        fout<<text;
+                    }   
+
+                 }
+                 
+             }
+        }
+        fin.close();
+        fout.close();
+        
+        if(!file_found)
+        {
+            cout<<"The given file is never copied into aggregator.txt";
+        }
+
 
     }
     else if((operation.compare("cut")) == 0) {
+        string old_filename = argv[2];
+        string new_filename = "cut_" + old_filename;
+        vector <string> buffer;
+        
+        fout.open(new_filename);
+        if(!fin) {
+            cout << "\n Invalid File Name. \n There is no such File or Directory ...";
+            exit(EXIT_FAILURE);
+        }
+        fin.open("aggregator.txt" , ios::app);
+        if(!fout) {
+            cout <<"\n Invalid File Name. \n There is no such File or Directory ...";
+            exit(EXIT_FAILURE);
+        }
+
+        string text;
+        bool file_found = false;
+        while(getline(fin, text)) {
+             buffer.push_back(text);
+             if((text.compare(argv[2])==0) && !file_found)
+             {
+                 buffer.pop_back();
+                 buffer.pop_back();
+                 file_found = true;
+                 while (getline(fin,text))
+                 {
+                    if(text.compare("//")==0)
+                    {
+                        buffer.push_back(text);
+                        break;
+                    }
+                    else
+                    {
+                        fout<<text;
+                    }   
+
+                 }
+                 
+             }
+        }
+        fin.close();
+        fout.close();
+
+        remove("aggregator.txt");
+        fout.open("aggregator.txt");
+        if(!fout) {
+            cout <<"\n Invalid File Name. \n There is no such File or Directory ...";
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+           for(int i=0; i<buffer.size(); i++)
+            {
+               fout<<buffer[i]<<"\n";
+            }
+            fout.close();
+        }
+        
+
+        if(!file_found)
+        {
+            cout<<"The given file is never copied into aggregator.txt";
+        }
 
     }
     else if((operation.compare("list")) == 0) {
