e04580256cd902529278ea76cb1e89af9117ebb5
Apurva Wani <apurva.wani06@gmail.com>
2020-07-09 18:12:11 +0530

    updated and comments added

--- main.cpp
+++ main.cpp
@@ -1,17 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-/**
- * To avoid change of file name in multiple places in code, it is stored in a variable 
-*/
+
+//To avoid change of file name in multiple places in code, it is stored in a variable 
 string main_file = "aggregator.txt";
 
 ifstream fin;
 ofstream fout;
 
 void add(string file_name) {
-    /**
-     * To keep track of total lines in the file to be added
-    */
+
+    //To keep track of total lines in the file to be added
     long long int total_lines = 0;
 
     fin.open(file_name);
@@ -25,10 +23,10 @@ void add(string file_name) {
         exit(EXIT_FAILURE);
     }
     string text;
-    /**
-     * '//' is added as a sign indicating start of a new file in aggregator.txt
-    */
+
+    // "//" is added as a sign indicating start of a new file in aggregator.txt
     fout << "//" << "\n";
+    
     fout << file_name << "\n\nContent:\n";
 
     fin.seekg(0, ios::end);
@@ -42,21 +40,17 @@ void add(string file_name) {
     }
     fout<<"\n\nMetadata:\n";
 
-    /**
-     * current date/time based on current system
-     */ 
+    //current date/time based on current system
     time_t now = time(0);
 
-    /**
-     * convert now to string form
-     */
+    //convert now to string form
     char* dt = ctime(&now);
 
     
 
     fout << "This file is added on time: " << dt;
     fout << "Total lines: "<< total_lines<<"\n";
-    fout<<"Size of the file is"<<" "<< file_size<<" "<<"bytes";
+    fout <<"Size of the file is"<<" "<< file_size<<" "<<"bytes";
     fout << "\n\n";
     remove(file_name.c_str());
     cout << "File added successfully!!!\n";
@@ -80,7 +74,10 @@ void copy(string old_filename) {
     }
 
     string text;
+    
+    //To handle test case : copy operation should not be performed before add operation 
     bool file_found = false;
+    
     while(getline(fin, text)) {
         if((text.compare(old_filename) == 0) && !file_found) {
             file_found = true;
@@ -97,10 +94,16 @@ void copy(string old_filename) {
     
     if(!file_found)
         cout << "The given file is never copied into aggregator.txt\n";
+
+    cout << "File copied successfully!!!\n";
 }
 
 void cut(string old_filename) {
+
+    //This new file helps to restore deleted contents(Recycle Bin for aggregator)
     string new_filename = "cut_" + old_filename;
+
+    //used to restore contents of aggregator.txt 
     vector <string> buffer;
     
     fout.open(new_filename);
@@ -115,7 +118,10 @@ void cut(string old_filename) {
     }
 
     string text;
+
+    //To handle test case : cut operation should not be performed before add operation
     bool file_found = false;
+    
     while(getline(fin, text)) {
         buffer.push_back(text);
         if((text.compare(old_filename) == 0) && !file_found) {
@@ -151,8 +157,10 @@ void cut(string old_filename) {
     }
     
     if(!file_found) {
-        cout << "The given file is never copied into aggregator.txt";
+        cout << "The given file is never copied into aggregator.txt\n";
     }
+
+    cout << "File removed successfully!!!\n";
 }
 
 void display_list() {
@@ -162,6 +170,7 @@ void display_list() {
         exit(EXIT_FAILURE);
     }
     string file_start = "//" , text;
+    cout << "List of files present in aggregator.txt : \n";
     while(getline(fin , text)) {
         if(text.compare(file_start) == 0) {
             getline(fin , text);
@@ -171,18 +180,26 @@ void display_list() {
 }
 
 int main(int argc , char* argv[]) {
+    
+    //To ensure proper command-line argument is provided 
     if(argc < 2) {
         cout << "Invalid command!\n";
         return 0;
     }
+
     string operation = argv[1];
+    
     if((operation.compare("add")) == 0)
         add(argv[2]);
+    
     else if((operation.compare("copy")) == 0)
         copy(argv[2]);
+    
     else if((operation.compare("cut")) == 0)
         cut(argv[2]);
+    
     else if((operation.compare("list")) == 0)
         display_list();
+    
     return 0;
 }
